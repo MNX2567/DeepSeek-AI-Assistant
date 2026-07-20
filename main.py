766dfdb055413940d64863c7a612e9af3317cbdb
MNX2567 <18636725420@163.com>
2026-07-20 17:29:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ import redis
 # 导入 LangChain 智能体组件
 from langchain_openai import ChatOpenAI
 from langchain.agents import AgentExecutor, create_tool_calling_agent
+# 如果依然报错，请尝试下面这种拆分导入，这在 0.2.x 版本中最稳妥：
+from langchain.agents import create_tool_calling_agent
+from langchain.agents import AgentExecutor
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.tools import tool
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
